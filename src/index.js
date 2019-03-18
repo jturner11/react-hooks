0ca95b3130ab343d5d105cab092735cb3ec2e490
Josh Turner <joshturner11@hotmail.co.uk>
2019-03-18 12:28:44 +0000

    Use state hooks

---
+++ src/index.js
@@ -0,0 +1,17 @@
+import * as React from 'react';
+import { render } from 'react-dom';
+
+import Example from './components/UseState';
+import UseEffect from './components/UseEffect';
+import '../src/style.css';
+
+render(
+    <div>
+        <h1 className="index-title">REACT HOOKS LEARNING</h1>
+        <h3 className="use-state-hook">Use state hook</h3>
+        <Example />
+        <h3 className="use-state-hook">Effect hooks</h3>
+        <UseEffect />
+    </div>,
+    document.getElementById('app')
+);
